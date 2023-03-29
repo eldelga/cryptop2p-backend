05bac924accd35c5d51773d3d1d3001fca7b9ab5
Sergio Delgadillo <sdelgadillo@farmkd.com>
2023-03-29 16:14:43 -0300

    first commit

---
+++ backend-cryptop2p/src/main/java/com/desapp/backendcryptop2p/BackendCryptop2pApplication.java
@@ -0,0 +1,13 @@
+package com.desapp.backendcryptop2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCryptop2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCryptop2pApplication.class, args);
+	}
+
+}
---
+++ backend-cryptop2p/src/test/java/com/desapp/backendcryptop2p/BackendCryptop2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desapp.backendcryptop2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCryptop2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
