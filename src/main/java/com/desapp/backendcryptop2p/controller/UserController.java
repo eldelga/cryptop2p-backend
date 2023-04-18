edbc743295ae3423a22b85505db957c2730b39c2
Sergio Delgadillo <sdelgadillo@farmkd.com>
2023-04-18 15:03:44 -0300

    added delete and update in endpoint

--- src/main/java/com/desapp/backendcryptop2p/controller/UserController.java
+++ src/main/java/com/desapp/backendcryptop2p/controller/UserController.java
@@ -3,8 +3,12 @@ package com.desapp.backendcryptop2p.controller;
 import com.desapp.backendcryptop2p.model.User;
 import com.desapp.backendcryptop2p.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
@@ -39,5 +43,15 @@ public class UserController {
     @PostMapping(value = "/user")
     public void save(@RequestBody User user) {
        this.userService.create(user);
+    }
+
+    @PutMapping(value = "/user")
+    public void update(@RequestBody User user) {
+        this.userService.create(user);
+    }
+
+    @DeleteMapping(value = "/user")
+    public void delete(@RequestParam Integer id) {
+        this.userService.delete(id);
     }
 }
