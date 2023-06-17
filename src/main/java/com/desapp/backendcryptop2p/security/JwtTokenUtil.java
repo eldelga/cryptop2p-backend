873b6aca2b765f28b5eb34837814cd05143c7abc
Sergio Delgadillo <sdelgadillo@farmkd.com>
2023-06-17 13:30:01 -0300

    added token barrer

--- src/main/java/com/desapp/backendcryptop2p/security/JwtTokenUtil.java
+++ src/main/java/com/desapp/backendcryptop2p/security/JwtTokenUtil.java
@@ -1,14 +1,24 @@
 package com.desapp.backendcryptop2p.security;
 
 import java.util.Date;
- 
+
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.desapp.backendcryptop2p.model.User;
 
+
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.jsonwebtoken.ExpiredJwtException;
+
 
  
 @Component
@@ -27,5 +37,37 @@ public class JwtTokenUtil {
                 .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
                 .compact();
                  
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenUtil.class);
+     
+    public boolean validateAccessToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException ex) {
+            LOGGER.error("JWT expired", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            LOGGER.error("Token is null, empty or only whitespace", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            LOGGER.error("JWT is invalid", ex);
+        } catch (UnsupportedJwtException ex) {
+            LOGGER.error("JWT is not supported", ex);
+        } catch (SignatureException ex) {
+            LOGGER.error("Signature validation failed");
+        }
+         
+        return false;
+    }
+     
+    public String getSubject(String token) {
+        return parseClaims(token).getSubject();
+    }
+     
+    private Claims parseClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
