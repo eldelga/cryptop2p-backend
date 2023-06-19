4bf0357703a35ca0b50c5d557049db167dace8f3
Sergio Delgadillo <sdelgadillo@farmkd.com>
2023-06-19 11:20:23 -0300

    extract email from authorization class

--- src/main/java/com/desapp/backendcryptop2p/controller/HelloController.java
+++ src/main/java/com/desapp/backendcryptop2p/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.desapp.backendcryptop2p.controller;
 
 
+import java.nio.file.attribute.UserPrincipal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +17,12 @@ import io.swagger.annotations.ApiImplicitParam;
 @RestController
 public class HelloController {
 
+
 	@ApiImplicitParam(name = "Authorization", value = "Access token", required = true, paramType = "header")
 	@GetMapping("/hello")
-	public String index(HttpServletRequest request) {
-		String token = (String) request.getAttribute("token");
-
-        // Aquí puedes usar el token como lo necesites en tu controlador
-
-        return "Token: " + token;
-
+	public String index(Authentication authCredential) {
+  		UserDetails userPrincipal = (UserDetails)authCredential.getPrincipal(); 
+		return userPrincipal.getUsername();
 	}
 
 }
